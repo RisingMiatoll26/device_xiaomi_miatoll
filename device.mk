71f11999c67a80f1898833c8c19838860d4b1f92
AbdoAhmedElbanaa <115952879+AbdoAhmedElbanaa@users.noreply.github.com>
2026-01-14 12:14:10 +0200

    Revert "miatoll: bringup clover project"
    This reverts commit 21d1f06c5b50bfef90f450e736e55f5cf7dd4c6a.

--- device.mk
+++ device.mk
@@ -190,7 +190,8 @@ PRODUCT_PACKAGES += \
     MiatollWifi
 
 PRODUCT_PACKAGES += \
-    MiatollAperture
+    MiatollAperture \
+    MiatollLineageSDK
 
 # Partitions
 PRODUCT_PACKAGES += \
@@ -294,7 +295,7 @@ PRODUCT_SOONG_NAMESPACES += \
     bootable/deprecated-ota \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/clover/interfaces/power-libperfmgr \
+    hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
 
